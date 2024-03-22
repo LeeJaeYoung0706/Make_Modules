9acdc6415b71333c987a73a295e60d0e85645af4
2년 차 개발자 이재영입니다 <148510209+LeeJaeYoung0706@users.noreply.github.com>
2024-03-22 10:48:08 +0900

    Create timer.js

---
+++ react/timer.js
@@ -0,0 +1,49 @@
+import {useEffect, useMemo, useState} from "react";
+
+const getSeconds = (time) => {
+    const seconds = Number(time % 60);
+    if(seconds < 10) {
+        return "0" + String(seconds);
+    } else {
+        return String(seconds);
+    }
+}
+const initTime = 30
+
+const Timer = ({handleIsShow , isShow , handleData}) => {
+    const [time, setTime] = useState(initTime); // 남은 시간 (단위: 초)
+
+    useEffect(() => {
+        if(isShow) {
+            const timer = setInterval(() => {
+                setTime((prev) => prev - 1);
+            }, 1000);
+
+            if (time < 0) {
+                handleIsShow(() => false)
+                setTime(() => initTime)
+                handleData( () => {
+                    authIdx: -1,
+                    phone: "",
+                    token: ""
+                });
+                setTimeout(() => {
+                    alert("시간이 초과되었습니다")
+                } , 200)
+            }
+            return () => clearInterval(timer);
+        }
+    }, [time , isShow]);
+    return (
+        <div>
+            <h1>남은 시간</h1>
+            <div>
+                <span>{parseInt(time / 60)}</span>
+                <span> : </span>
+                <span>{getSeconds(time)}</span>
+            </div>
+        </div>
+    );
+}
+
+export default Timer;
